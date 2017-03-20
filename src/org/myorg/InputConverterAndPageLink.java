d9053c79ed179493636e9beb1a1ca285111062fa
cloudera <cloudera@quickstart.cloudera>
2017-03-20 08:09:38 -0700

    ...

--- src/org/myorg/InputConverterAndPageLink.java
+++ src/org/myorg/InputConverterAndPageLink.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 import java.io.IOException;
+import java.math.BigDecimal;
+
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -15,6 +17,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.log4j.Logger;
@@ -23,10 +26,10 @@ public class InputConverterAndPageLink extends Configured implements Tool {
 
 	private static final Logger LOG = Logger.getLogger(InputConverterAndPageLink.class);
 
-	private static int count_from_nodes = 0;
-	private static int nodeStartGetal = 0;
-	private static int totaalAantalNodes = 0;
-	private static int HETGetal = 1000;
+	private static BigDecimal count_to_nodes = new BigDecimal(0);
+	private static BigDecimal totaalAantalNodes = new BigDecimal(62586);
+	private static BigDecimal HETGetal = new BigDecimal(1000);
+	private static BigDecimal total = new BigDecimal(0);
 
 	public static void main(String[] args) throws Exception {
 		int res = ToolRunner.run(new InputConverterAndPageLink(), args);
@@ -50,11 +53,11 @@ public class InputConverterAndPageLink extends Configured implements Tool {
 		FileInputFormat.addInputPath(job2, new Path(args[1]));
 		FileOutputFormat.setOutputPath(job2, new Path(args[2]));
 		job2.setMapperClass(Map_PageLink.class);
-		// job2.setCombinerClass(Combiner.class);
+		job2.setCombinerClass(Reduce_PageLink.class);
 		job2.setReducerClass(Reduce_PageLink.class);
 		job2.setOutputKeyClass(Text.class);
 		job2.setOutputValueClass(Text.class);
-
+        
 		return job2.waitForCompletion(true) ? 0 : 1;
 	}
 
@@ -86,7 +89,6 @@ public class InputConverterAndPageLink extends Configured implements Tool {
 				toNode = new Text(word);
 				if (!toNode.equals(fromNode)) {
 					context.write(fromNode, toNode);
-					totaalAantalNodes++;
 				}
 			}
 		}
@@ -115,14 +117,13 @@ public class InputConverterAndPageLink extends Configured implements Tool {
 				Text fromNodeEntry = hashMapEntry.getKey();
 				List<Text> toNodesEntry = hashMapEntry.getValue();
 				context.write(fromNodeEntry, toNodesEntry);
+				//totaalAantalNodes = totaalAantalNodes.add(new BigDecimal(1));
 			}
 		}
 	}
 
 	public static class Map_PageLink extends Mapper<LongWritable, Text, Text, Text> {
-		private Text word = new Text();
 		private String input;
-
 		protected void setup(Mapper.Context context) throws IOException,
 				InterruptedException {
 			if (context.getInputSplit() instanceof FileSplit) {
@@ -136,57 +137,38 @@ public class InputConverterAndPageLink extends Configured implements Tool {
 		public void map(LongWritable offset, Text lineText, Context context)
 				throws IOException, InterruptedException {
 			String line = lineText.toString();
-			Text toNode = new Text();
 			Text fromNode = new Text(line.split("\t")[0]);
-			Text valueNode = new Text();
-
-			count_from_nodes = 0;
-			;
-			for (String word : line.split("\t")) {
-				count_from_nodes++;
-				if (word.isEmpty()) {
-					continue;
-				}
+			String toNodes = line.split("\t")[1];
+			Text pageLinkValueNode = new Text();
+			
+			toNodes = toNodes.replace("[","").replace("]", "");
+			count_to_nodes = new BigDecimal(0);
+			
+			for(String node : toNodes.split(", ")){
+				count_to_nodes = count_to_nodes.add(new BigDecimal(1));
 			}
-			double value = HETGetal / totaalAantalNodes / count_from_nodes;
-			valueNode = new Text(value + "");
-			for (String word : line.split("\t")) {
-				count_from_nodes++;
-				if (word.isEmpty()) {
-					continue;
-				}
-				toNode = new Text(word);
-				if (!toNode.equals(fromNode)) {
-					context.write(toNode, valueNode);
-				}
+			
+			BigDecimal firstDivide = HETGetal.divide(totaalAantalNodes,20,BigDecimal.ROUND_DOWN);
+			BigDecimal value = firstDivide.divide(count_to_nodes,20,BigDecimal.ROUND_DOWN);
+			value.add(firstDivide);l
+			pageLinkValueNode = new Text(value + "");
+			
+			for(String node : toNodes.split(", ")){
+				context.write(new Text(node), pageLinkValueNode);
 			}
 		}
 	}
 
-	public static class Reduce_PageLink extends Reducer<Text, Text, Text, List<Text>> {
-		public void reduce(Text fromNode, Iterable<Text> toNodes, Context context) throws IOException, InterruptedException {
-        /*
-			HashMap<Text, List<Text>> fromNodeToNodes = new HashMap<Text, List<Text>>();
-			List<Text> theToNodes = new ArrayList<Text>();
-
-			for (Text toNode : toNodes) {
-				Text insertToNode = new Text(toNode);
-				if (fromNodeToNodes.get(fromNode) == null
-						&& fromNodeToNodes.get(toNode) == null) {
-					fromNodeToNodes.put(fromNode, theToNodes);
-					fromNodeToNodes.get(fromNode).add(insertToNode);
-				} else {
-					fromNodeToNodes.get(fromNode).add(insertToNode);
-				}
+	public static class Reduce_PageLink extends Reducer<Text, Text, Text, Text> {
+		public void reduce(Text toNode, Iterable<Text> pageLinkValueNode, Context context) throws IOException, InterruptedException {
+			BigDecimal sum = new BigDecimal(0);
+			for(Text pagelinkValue : pageLinkValueNode){
+				BigDecimal value = new BigDecimal(pagelinkValue.toString().replaceAll(",",""));
+				sum = sum.add(value);
 			}
-
-			for (Entry<Text, List<Text>> hashMapEntry : fromNodeToNodes
-					.entrySet()) {
-				Text fromNodeEntry = hashMapEntry.getKey();
-				List<Text> toNodesEntry = hashMapEntry.getValue();
-				context.write(fromNodeEntry, toNodesEntry);
-			}
-          */
+			total = total.add(sum);
+			System.out.println(total);
+			context.write(toNode, new Text(sum+""));
 		}
 	}
 }
